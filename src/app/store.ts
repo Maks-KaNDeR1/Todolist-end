bb003ca6d57c583dbd55057328119f8cfb671b35
Maks-KaNDeR <makskander1@gmail.com>
2022-04-12 22:08:35 +0300

    todov15

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,7 @@
-import { tasksReducer } from './tasks-reducer';
-import { todolistsReducer } from './todolists-reducer';
-import { applyMiddleware, combineReducers, createStore} from 'redux'
+import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
+import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
+import {applyMiddleware, combineReducers, createStore} from 'redux'
 import thunkMiddleware from 'redux-thunk'
-
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -18,4 +17,3 @@ export type AppRootStateType = ReturnType<typeof rootReducer>
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store;
-
